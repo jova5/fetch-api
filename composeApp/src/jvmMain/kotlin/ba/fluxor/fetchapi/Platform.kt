b49ca69cd36bba178cea19e78665c7dfb139863e
Miloš Jovičić <jovicicm55@outlook.com>
2026-04-10 13:32:11 +0200

    init

---
+++ composeApp/src/jvmMain/kotlin/ba/fluxor/fetchapi/Platform.kt
@@ -0,0 +1,7 @@
+package ba.fluxor.fetchapi
+
+class JVMPlatform {
+  val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+fun getPlatform() = JVMPlatform()
---
+++ composeApp/src/jvmMain/kotlin/ba/fluxor/fetchapi/main.kt
@@ -0,0 +1,13 @@
+package ba.fluxor.fetchapi
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+  Window(
+    onCloseRequest = ::exitApplication,
+    title = "fetchapi",
+  ) {
+    App()
+  }
+}
