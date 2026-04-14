2187eff950a5e3ecdd71ba1b98d08db6183e8052
Miloš Jovičić <jovicicm55@outlook.com>
2026-04-14 18:39:57 +0200

    remove init files

--- composeApp/src/jvmMain/kotlin/ba/fluxor/fetchapi/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package ba.fluxor.fetchapi
-
-class Greeting {
-  private val platform = getPlatform()
-
-  fun greet(): String {
-    return "Hello, ${platform.name}!"
-  }
-}
--- composeApp/src/jvmMain/kotlin/ba/fluxor/fetchapi/Platform.kt
+++
@@ -1,7 +0,0 @@
-package ba.fluxor.fetchapi
-
-class JVMPlatform {
-  val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-fun getPlatform() = JVMPlatform()
